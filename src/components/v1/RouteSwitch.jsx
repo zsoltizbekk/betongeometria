107fafaec2d4b58ce5fc251e45544460db173f64
Zsolt Török <zsolt3210@gmail.com>
2022-11-22 21:48:35 +0100

    .

--- src/components/v1/RouteSwitch.jsx
+++ src/components/v1/RouteSwitch.jsx
@@ -14,15 +14,15 @@ const RouteSwitch = () => {
     return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<StartingPage />} />
-            <Route path="/epitesz" element={<Epitesz />} />
-            <Route path="/beton" element={<Beton />} />
-            <Route path="/aszf" element={<Aszf />} />
-            <Route path="/gdpr" element={<Gdpr />} />
-            <Route path="/joginyilatkozat" element={<JogiNyilatkozat />} />
-            <Route path="/butor" element={<Butor />} />
-            <Route path="/falburkolat" element={<Falburkolat />} />
-            <Route path="/kulterielemek" element={<Kulterielemek />} />
+            <Route path="/betongeometria/" element={<StartingPage />} />
+            <Route path="/betongeometria/epitesz" element={<Epitesz />} />
+            <Route path="/betongeometria/beton" element={<Beton />} />
+            <Route path="/betongeometria/aszf" element={<Aszf />} />
+            <Route path="/betongeometria/gdpr" element={<Gdpr />} />
+            <Route path="/betongeometria/joginyilatkozat" element={<JogiNyilatkozat />} />
+            <Route path="/betongeometria/butor" element={<Butor />} />
+            <Route path="/betongeometria/falburkolat" element={<Falburkolat />} />
+            <Route path="/betongeometria/kulterielemek" element={<Kulterielemek />} />
 
           </Routes>
         </BrowserRouter>
